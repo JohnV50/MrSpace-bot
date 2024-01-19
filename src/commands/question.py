e63c63391c248d459d051a5e3a919ace51044ab5
lxphere <47260747+lxphere@users.noreply.github.com>
2024-01-19 17:34:01 -0500

    fix question cmd yay

--- src/commands/question.py
+++ src/commands/question.py
@@ -1,23 +1,23 @@
 import discord
 from discord.ext import commands
-
+import constants
 
 @commands.command()
 async def question(ctx, *, question):
     try:
-        if ctx.message.author.guild_permissions.manage_guild == False:
+        if constants.ADMIN_ROLES in [role.id for role in ctx.message.author.roles]:
             return await ctx.send("You don't have permission to run this command.")
         # send the question embed
         send_embed = discord.Embed(
             title = "Question of the Day",
-            description = question + "\n\nTell me in answers",
+            description = question + "\n\nTell me in <#993990816693506110>!",
             color = 0x7289da,
             )
 
         # add new fields
         send_embed.add_field(name = "Sent by", value = ctx.author.mention, inline = False)
-                   
-        await ctx.send(content="<@194962036784889858> L",embed=send_embed)
+        channel = ctx.guild.get_channel(constants.QUESTION_CHANNEL)          
+        await channel.send(content="<@&923787096018255953>",embed=send_embed)
 
 
     # exception handling
