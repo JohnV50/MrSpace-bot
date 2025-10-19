e591a6a8c712d92887a18a8f5493e28357b1e638
John <42081256+JohnV50@users.noreply.github.com>
2025-10-19 11:00:58 -0400

    updates

--- src/commands/sendcmds/sendrulestwo.py
+++ src/commands/sendcmds/sendrulestwo.py
@@ -14,12 +14,12 @@ async def sendrulestwo(ctx):
             )
 
         # add new fields
-        send_embed.add_field(name = "", value = "**11.** Do not misuse the support tickets." + "\n" +
-        "**10.** You must speak English so we can moderate properly." + "\n" +
-        "**11.** Copy pasted and chain mail are prohibited." + "\n" +
-        "**12.** Don't spread misinformation." + "\n" +
-        "**13.** Using alternative accounts for an unfair advantage in bots, events, giveaways, or anything along those lines is prohibited." + "\n" +
-        "**14.** Do not make anyone feel uncomfortable, be respectful to all." + "\n" +
+        send_embed.add_field(name = "", value = "**10.** Don't misuse the support tickets." + "\n" +
+        "**11.** You must speak English so we can moderate properly." + "\n" +
+        "**12.** Copy pasted and chain mail are prohibited." + "\n" +
+        "**13.** Don't spread misinformation." + "\n" +
+        "**14.** Using alternative accounts for an unfair advantage in bots, events, giveaways, or anything along those lines is prohibited." + "\n" +
+        "**15.** Do not make anyone feel uncomfortable, be respectful to all." + "\n" +
         "Violation of any of these rules may result in removal from the server. Use common sense, don't do anything you aren't supposed to be doing.", inline = False)
                    
         await ctx.send(embed=send_embed)
---
+++ src/commands/timer.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+
+@commands.command()
+async def timer(ctx):
+    ping = round((ctx.bot.latency) * 1000)
+    await ctx.send("Pong! - " + str(ping) + "ms")
+
+
+# connecting to main file
+
+async def setup(bot):
+    bot.add_command(timer)
